05acc574491ccc66e3a045931e1e0e81bfd4f179
Ola <oladabro@gmail.com>
2021-02-18 19:52:40 +0100

    fetch data from db.json, inject data in index.html

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
+const container = document.querySelector('.recipes');
+
 const renderRecipes = async () => {
-  let uri = 'http://localhost:3000';
+  let uri = 'http://localhost:3000/recipes';
 
   const res = await fetch(uri);
+  const recipes = await res.json();
+
+  let template = '';
+
+  recipes.forEach((recipe) => {
+    template += `
+    <div class='recipe'>
+      <h2>${recipe.name}</h2>
+      <p>${recipe.instructions.slice(0, 200)}</p>
+      <a href="/details.html?id=${recipe.id}">read more...</a>
+    </div>
+    `;
+  });
+
+  container.innerHTML = template;
 };
 
 window.addEventListener('DOMContentLoaded', () => renderRecipes());
