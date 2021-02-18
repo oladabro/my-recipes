0ac72afde6b8a073d8189cbb3621bbe7ec557544
Ola <oladabro@gmail.com>
2021-02-18 18:29:48 +0100

    create html, js, json and css files. initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const renderRecipes = async () => {
+  let uri = 'http://localhost:3000';
+
+  const res = await fetch(uri);
+};
+
+window.addEventListener('DOMContentLoaded', () => renderRecipes());
